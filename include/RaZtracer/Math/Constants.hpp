3effa535a3b468d066fe0ecdf669b56abb41444d
Romain Milbert <romain.milbert@gmail.com>
2018-01-11 13:10:54 +0100

    [Add] Added Constants file

---
+++ include/RaZtracer/Math/Constants.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#ifndef RAZTRACER_CONSTANTS_HPP
+#define RAZTRACER_CONSTANTS_HPP
+
+template <typename T>
+constexpr T pi = T(3.14159265358979323846L);
+
+#endif // RAZTRACER_CONSTANTS_HPP
