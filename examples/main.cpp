34892c53de81085df5fdf431415b9b560c1514ae
Romain Milbert <romain.milbert@gmail.com>
2018-01-25 12:55:21 +0100

    [Update] Changed main, creating unique_ptrs & adding a camera

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,22 +1,14 @@
-#include <iostream>
-
-#include "RaZtracer/Math/Vector.hpp"
-#include "RaZtracer/Render/Scene.hpp"
-#include "RaZtracer/Utils/Shape.hpp"
+#include "RaZtracer/RaZtracer.hpp"
 
 int main() {
   Scene scene;
-  scene.addShape(Sphere(Vec3f({ 1.5, 2.5, 3.5 }), 1.f, Vec3f({ 1.0, 1.0, 1.0 }), 1.f));
-  scene.addShape(Cube(Vec3f({ 3.5, 1.5, 5.5 }), 1.f, Vec3f({ 1.0, 0.5, 0.2 }), 0.5f));
+  scene.addShape(std::make_unique<Sphere>(Vec3f({ 1.5f, 2.5f, 3.5f }), 1.f));
+  scene.addShape(std::make_unique<Cube>(Vec3f({ 3.5f, 1.5f, 5.5f }), 1.f));
+  //scene.addShape(std::make_unique<Triangle>(Vec3f({  })));
 
-  Vec3f vec1({ 5, 3, 4 }), vec2({ -2, 5, -3 });
-  vec1 = vec1.cross(vec2);
-  std::cout << vec1[0] << ' ' << vec1[1] << ' ' << vec1[2] << std::endl;
+  scene.setCamera(std::make_unique<Camera>(800, 600, 45.f, 0.1f, 100.f, Vec3f({ 0.f, 0.f, -10.f })));
 
-  vec1 = vec1.normalize();
-  std::cout << vec1[0] << ' ' << vec1[1] << ' ' << vec1[2] << std::endl;
-
-  scene.render("output.png");
+  scene.render("output.png", 1920, 1080);
 
   return 0;
 }
