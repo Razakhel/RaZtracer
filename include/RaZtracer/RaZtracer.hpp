cafb38a8edad053301ed3d11af56133602a4ce9c
Romain Milbert <romain.milbert@gmail.com>
2018-01-25 12:54:07 +0100

    [Add] Added global header

---
+++ include/RaZtracer/RaZtracer.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifndef RAZTRACER_RAZTRACER_HPP
+#define RAZTRACER_RAZTRACER_HPP
+
+#include "Math/Matrix.hpp"
+#include "Math/Quaternion.hpp"
+#include "Math/Transform.hpp"
+#include "Math/Vector.hpp"
+#include "Render/Camera.hpp"
+#include "Render/Light.hpp"
+#include "Render/Scene.hpp"
+#include "Utils/Image.hpp"
+#include "Utils/Window.hpp"
+
+#endif // RAZTRACER_RAZTRACER_HPP
