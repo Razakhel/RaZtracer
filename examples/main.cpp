55dcb6f71161a2980a4da8114a5c3b86dacbc958
Romain Milbert <romain.milbert@gmail.com>
2018-02-05 14:19:39 +0100

    [Update] Main program now displays a white Sphere at the center [Update] Added a chrono to compute rendering time

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,14 +1,21 @@
+#include <chrono>
+#include <iostream>
+
 #include "RaZtracer/RaZtracer.hpp"
 
 int main() {
   Scene scene;
-  scene.addShape(std::make_unique<Sphere>(Vec3f({ 1.5f, 2.5f, 3.5f }), 1.f));
-  scene.addShape(std::make_unique<Cube>(Vec3f({ 3.5f, 1.5f, 5.5f }), 1.f));
-  //scene.addShape(std::make_unique<Triangle>(Vec3f({  })));
+  scene.addShape(std::make_unique<Sphere>(Vec3f(0.f), 1.f));
 
-  scene.setCamera(std::make_unique<Camera>(800, 600, 45.f, 0.1f, 100.f, Vec3f({ 0.f, 0.f, -10.f })));
+  scene.setCamera(std::make_unique<Camera>(800, 600, 45.f, 0.1f, 100.f, Vec3f({ 0.f, 0.f, 5.f })));
 
-  scene.render("output.png", 1920, 1080);
+  const auto startTime = std::chrono::system_clock::now();
+  scene.render("output.png");
+  const auto endTime = std::chrono::system_clock::now();
+
+  std::cout << "Scene rendering duration: "
+            << std::chrono::duration_cast<std::chrono::duration<float>>(endTime - startTime).count()
+            << " seconds." << std::endl;
 
   return 0;
 }
