c57f5da4fa33896ad699d54c357297c1e216bfca
Romain Milbert <romain.milbert@gmail.com>
2018-02-06 09:29:43 +0100

    [Update] Main program now displays result onto a Window

--- examples/main.cpp
+++ examples/main.cpp
@@ -4,18 +4,32 @@
 #include "RaZtracer/RaZtracer.hpp"
 
 int main() {
+  Window window(800, 600, "RaZtracer");
+
   Scene scene;
   scene.addShape(std::make_unique<Sphere>(Vec3f(0.f), 1.f));
+  scene.addShape(std::make_unique<Sphere>(Vec3f({ 2.f, 1.f, 3.f }), 1.f));
+  scene.addShape(std::make_unique<Sphere>(Vec3f({ 1.f, 2.f, 2.f }), 1.f));
+  scene.addShape(std::make_unique<Sphere>(Vec3f({ 2.f, 3.f, 0.f }), 1.f));
+  scene.addShape(std::make_unique<Sphere>(Vec3f({ 1.f, 1.5f, 0.f }), 1.f));
+  scene.addShape(std::make_unique<Sphere>(Vec3f({ 2.f, 0.5f, 1.f }), 1.f));
+  scene.addShape(std::make_unique<Sphere>(Vec3f({ 3.f, 4.f, 3.f }), 1.f));
+  //scene.addShape(std::make_unique<Cube>(Vec3f({ 3.5f, 1.5f, 5.5f }), 1.f));
+  //scene.addShape(std::make_unique<Triangle>(Vec3f({  })));
 
   scene.setCamera(std::make_unique<Camera>(800, 600, 45.f, 0.1f, 100.f, Vec3f({ 0.f, 0.f, 5.f })));
 
   const auto startTime = std::chrono::system_clock::now();
-  scene.render("output.png");
+  std::unique_ptr<Image> img = scene.render("output.png");
   const auto endTime = std::chrono::system_clock::now();
 
   std::cout << "Scene rendering duration: "
             << std::chrono::duration_cast<std::chrono::duration<float>>(endTime - startTime).count()
             << " seconds." << std::endl;
+
+  window.mapImage(std::move(img));
+
+  while (window.show());
 
   return 0;
 }
