a5d5d9da8488af3f3615c332464ac7cb2ac8fe4a
Razakhel <romain.milbert@gmail.com>
2018-03-07 13:24:16 +0100

    [Add] Added Vertex struct

---
+++ include/RaZtracer/Utils/Vertex.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifndef RAZTRACER_VERTEX_HPP
+#define RAZTRACER_VERTEX_HPP
+
+#include "../Math/Vector.hpp"
+
+struct Vertex {
+  explicit Vertex(const Vec3f& vertPosition) : position{ vertPosition } {}
+  Vertex(const Vec3f& vertPosition, const Vec3f& vertNormal) : position{ vertPosition }, normal{ vertNormal } {}
+
+  Vec3f position {};
+  Vec3f normal {};
+  Vec2f texcoords {};
+};
+
+#endif // RAZTRACER_VERTEX_HPP
