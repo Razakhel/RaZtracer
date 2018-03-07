3254812771fc7afbe21f01d41e5354eda511a7c4
Razakhel <romain.milbert@gmail.com>
2018-03-07 13:30:28 +0100

    [Update] Main example now loads a simple OBJ ball [Update] Inverted camera position

--- examples/main.cpp
+++ examples/main.cpp
@@ -8,22 +8,11 @@ int main() {
 
   Scene scene;
 
-  scene.addShape(std::make_unique<Sphere>(Vec3f(0.f), 1.f));
-  scene.addShape(std::make_unique<Sphere>(Vec3f({ 0.f, 0.f, 3.f }), 1.f, Vec3f({ 1.f, 0.f, 0.f })));
-  scene.addShape(std::make_unique<Sphere>(Vec3f({ 2.f, 1.f, 3.f }), 1.f, Vec3f({ 1.f, 0.f, 0.f })));
-  scene.addShape(std::make_unique<Sphere>(Vec3f({ 1.f, -2.f, 2.f }), 1.f, Vec3f({ 0.f, 1.f, 1.f })));
-  scene.addShape(std::make_unique<Sphere>(Vec3f({ -2.f, 3.f, 0.f }), 1.f, Vec3f({ 1.f, 1.f, 0.f })));
-  scene.addShape(std::make_unique<Sphere>(Vec3f({ -1.f, 1.5f, 0.f }), 1.f, Vec3f({ 0.f, 1.f, 0.f })));
-  scene.addShape(std::make_unique<Sphere>(Vec3f({ 2.f, -0.5f, 1.f }), 1.f, Vec3f({ 1.f, 1.f, 0.f })));
-  scene.addShape(std::make_unique<Sphere>(Vec3f({ -3.f, -2.f, 3.f }), 1.f, Vec3f({ 1.f, 0.f, 1.f })));
-
-  scene.addShape(std::make_unique<Triangle>(Vec3f({ 6.f, 0.f, 3.f }), Vec3f({ 3.f, -1.f, 3.f}), Vec3f({ 1.5f, 2.f, 3.f })));
-
-  scene.addShape(std::make_unique<Box>(Vec3f({ 5.f, 3.f, 3.f }), Vec3f({ 2.f, 1.f, 5.f })));
+  scene.addModel("../assets/meshes/ball.obj");
 
   //scene.addLight(std::make_unique<PointLight>(Vec3f({ 0.f, 1.f, 0.f }), Vec3f(1.f)));
 
-  scene.setCamera(std::make_unique<Camera>(800, 600, 45.f, 0.1f, 100.f, Vec3f({ 0.f, 0.f, -5.f })));
+  scene.setCamera(std::make_unique<Camera>(800, 600, 45.f, 0.1f, 100.f, Vec3f({ 0.f, 0.f, 5.f })));
 
   const auto startTime = std::chrono::system_clock::now();
   ImagePtr img = scene.render();
